6bcd6f08da049f3f444e58d5f012e8dda0323cb4
rphan <rphan@genomichealth.com>
2018-11-07 14:33:02 -0800

    Complete implementing stopwatch with all functionalities

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,11 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var startButton: UIButton!
+    var data = [String]()
+    var timer = Timer()
+    var second = 0
+  
+    
+    @IBOutlet weak var secondLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if (indexPath.row < data.count) {
+            cell.textLabel?.text = data[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = ""
+        }
+        return cell
+    }
+    @objc func updateTime() {
+        second += 1;
+        secondLabel.text = timeString(time:TimeInterval(second))
+    }
+    @IBAction func start(_ sender: UIButton) {
+        if (!timer.isValid) {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(ViewController.updateTime), userInfo: nil, repeats: true)
+            startButton.setTitle("Lap", for: UIControl.State.normal)
+        }
+        else {
+            data.append(timeString(time:TimeInterval(second)))
+            tableView.reloadData()
+        }
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        timer.invalidate()
+        second = 0
+        secondLabel.text = "\(0)"
+        startButton.setTitle("Start", for: UIControl.State.normal)
+        data = [String]()
+        tableView.reloadData()
+    }
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String.init(format: "%02i:%02i:%02i", hours, minutes, seconds)
     }
 
 
